198b1cc0f9668917936c01374093b5695c282253
maximeri <t.s.eliot1996@gmail.com>
2022-06-17 17:01:36 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const port = 3000
+const session = require('express-session')
+const passport = require('./config/passport')
+const SESSION_SECRET = 'secret'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api', routes)
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
+
+module.exports = app
