411473adef953ceaed1516daab5434093d88ba82
maximeri <t.s.eliot1996@gmail.com>
2022-06-25 15:58:42 +0800

    feat: add likedbook medel

---
+++ migrations/20220625075813-create-liked-book.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('LikedBooks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      book_id: {
+        type: Sequelize.NUMBER
+      },
+      user_id: {
+        type: Sequelize.NUMBER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('LikedBooks');
+  }
+};
---
+++ models/likedbook.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class LikedBook extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  LikedBook.init({
+    book_id: DataTypes.NUMBER,
+    user_id: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'LikedBook',
+    underscored: true,
+  });
+  return LikedBook;
+};
