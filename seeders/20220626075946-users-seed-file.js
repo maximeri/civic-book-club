586bc82eab90c59155b7533a7a45bb0845d04b2e
maximeri <t.s.eliot1996@gmail.com>
2022-06-26 16:42:09 +0800

    feat:add user seeders

---
+++ seeders/20220626075946-users-seed-file.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+function generateUsers() {
+  const arr = []
+  for (let i = 0; i < 25; i++) {
+    arr[i] = {
+      id: i + 1,
+      name: `user${i + 1}`,
+      email: `user${i + 1}@example.com`,
+      password: bcrypt.hashSync('12345678', 10),
+      account: `user${i + 1}`,
+      preference: faker.lorem.sentence(),
+      job: faker.lorem.sentence(),
+      goal: faker.lorem.sentence(),
+      avatar: `https://loremflickr.com/320/240/people/?random=${Math.random() * 100}`,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+  }
+  return arr
+}
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users',
+      generateUsers()
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
