c524927e8845247aa290b16571b6cf15919cc538
maximeri <t.s.eliot1996@gmail.com>
2022-06-27 18:04:55 +0800

    add friendship routes

---
+++ routes/modules/friendships.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const friendshipController = require('../../controllers/friendship-controller')
+router.post('/:userId', friendshipController.sendRequest) // 送出交友邀請
+router.delete('/:userId', friendshipController.cancelRequest) // 收回交友邀請
+router.post('/:id', friendshipController.respondRequest) // 回覆交友邀請
