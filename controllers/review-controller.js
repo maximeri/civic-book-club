2fcb30fa0947324462cbd23184a57b74e76715b0
maximeri <t.s.eliot1996@gmail.com>
2022-06-29 16:10:09 +0800

    feat: add review controllers

---
+++ controllers/review-controller.js
@@ -0,0 +1,98 @@
+const { Book, User, Review, LikedReview } = require('../models')
+
+const reviewController = {
+  getReviews: (req, res, next) => {
+      return Review.findAndCountAll({
+        include: [
+          { model: User, as: 'LikedReviews', attributes: ['id'] }
+        ],
+        order: [['createdAt', 'DESC']]
+      })
+        .then(reviews => {
+          const likedReviewId = req.user?.LikedReviews ? req.user.LikedReviews.map(likeReview => likeReview.id) : []
+          const resultReviews = reviews.rows.map(r => ({
+            ...r.toJSON(),
+            isLiked: likedReviewId.includes(r.id),
+            totalLikes: r.likeReviews ? r.likeReviews.length : 0
+          }))
+          return res.json(resultReviews)
+        })
+        .catch(err => next(err))
+  },
+  addReview: (req, res, next) => {
+    const title = req.body.title
+    const content = req.body.content
+    if (!title || !content) throw new Error('Field required!')
+    return Review.create({
+      UserId: req.user.id,
+      title,
+      content
+    })
+      .then(review => {
+        return res.json(review)
+      })
+      .catch(err => next(err))
+  },
+  editReview: (req, res, next) => {
+    const title = req.body.title || null
+    const content = req.body?.content || null
+    if (!title || !content) throw new Error('Field required!')
+      return Review.findByPk(req.params.id)
+      .then(review => {
+        if (!review) throw new Error('Review does not exist')
+        return review.update({
+          title,
+          content
+        })
+      })
+      .then(review => {
+        review = review.toJSON()
+        return res.json(review)
+      })
+      .catch(err => next(err))
+  },
+  deleteReview: (req, res, next) => {
+    const userId = req.user.id
+    const reviewId = req.params.id
+    try {
+      const review = await Review.findOne({
+        where: { id: userId, reviewId }
+      })
+      if (!review) throw new Error("Review doesn't exist or you don't have permission to edit!")
+      const destroyedReview = await review.destroy()
+      await LikedReview.destroy({
+        where: { reviewId }
+      })
+      res.json(destroyedReview)
+    } catch (err) {
+      next(err)
+    }
+  },
+  likeReview: (req, res, next) => {
+    const userId = req.user.id
+    const reviewId = Number(req.params.id)
+    return Promise.all([
+      Review.findByPk(reviewId),
+      LikedReview.findOne({
+        where: {
+          userId,
+          reviewId
+        }
+      })
+    ])
+      .then(([review, like]) => {
+        if (!review) throw new Error("Review doesn't exist!")
+        if (like) throw new Error('You have liked this review!')
+        return Review.create({
+          userId,
+          reviewId
+        })
+      })
+      .then(likedReview => {
+        return res.json(likedReview)
+      })
+      .catch(err => next(err))
+  }
+}
+
+module.exports = reviewController
--- routes/modules/reviews.js
+++ routes/modules/reviews.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const reviewController = require('../../controllers/review-controller')
+const reviewController = require('../../controllers/review-controller.js')
 router.post('/:userId', reviewController.likeReview) // 按讚書評
 router.post('/', reviewController.addReview) // 新增書評
 router.put('/:id', reviewController.editReview) // 修改書評
 router.delete('/:id', reviewController.deleteReview) // 刪除書評
-router.get('/:bookId?sortBy=ctime&limit=10', reviewController.getNewReviews) // 查看一
 router.get('/:userId', reviewController.getReviews) // 查看此用戶所有書評
 
 module.exports = router
