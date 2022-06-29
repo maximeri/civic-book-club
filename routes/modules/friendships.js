cc231a380b2ff665b66ff91c0f5b893fd9330d09
maximeri <t.s.eliot1996@gmail.com>
2022-06-29 16:20:48 +0800

    fix: add exports

--- routes/modules/friendships.js
+++ routes/modules/friendships.js
@@ -4,3 +4,5 @@ const friendshipController = require('../../controllers/friendship-controller')
 router.post('/:userId', friendshipController.sendRequest) // 送出交友邀請
 router.delete('/:userId', friendshipController.cancelRequest) // 收回交友邀請
 router.post('/:id', friendshipController.respondRequest) // 回覆交友邀請
+
+module.exports = router
