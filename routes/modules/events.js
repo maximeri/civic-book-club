9091f9c876c0290b8c3ee7986a09ecf6dc4f6e17
maximeri <t.s.eliot1996@gmail.com>
2022-06-30 18:02:25 +0800

    feat: add event controllers

--- routes/modules/events.js
+++ routes/modules/events.js
@@ -8,6 +8,5 @@ router.delete('/:id', eventController.deleteEvent) // 刪除讀書會
 router.post('/:id', eventController.joinEvent) // 加入讀書會
 router.delete('/:id/userId', eventController.leaveEvent) // 取消加入讀書會
 router.get('/:bookId', eventController.getEvents) // 查看該書所有讀書會
-router.get('/:id', eventController.getEvent) // 查看個別讀書會資訊
 
 module.exports = router
