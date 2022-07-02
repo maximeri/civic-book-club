f6a665e5b2cbd8f36aa16b96bb49e64421883255
maximeri <t.s.eliot1996@gmail.com>
2022-07-02 14:42:25 +0800

    feat:add event model

--- models/event.js
+++ models/event.js
@@ -5,25 +5,26 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
     static associate(models) {
-      Event.hasMany(models.User, { foreignKey: 'HostId' })
-      Event.hasMany(models.Book, { foreignKey: 'BookId' })
-      Event.belongsToMany(models.User, { through: models.Participation, foreignKey: 'EventId' })
+      Event.belongsTo(models.User, {foreignKey: 'HostId'})
+      Event.belongsTo(models.Book, { foreignKey: 'BookId'})
+      Event.belongsToMany(models.User, { through: models.Participation ,foreignKey: 'EventId' })
     }
   }
   Event.init({
     topic: DataTypes.STRING,
-    start_at: DataTypes.DATE,
-    end_at: DataTypes.DATE,
-    memberCount: DataTypes.NUMBER,
+    startAt: DataTypes.DATE,
+    endAt: DataTypes.DATE,
+    memberCount: DataTypes.INTEGER,
     meetingLink: DataTypes.STRING,
     isPublished: DataTypes.BOOLEAN,
     isPrivate: DataTypes.BOOLEAN,
-    host_id: DataTypes.NUMBER,
-    book_id: DataTypes.NUMBER,
-    currentMemberCount: DataTypes.NUMBER
+    currentMemberCount: DataTypes.INTEGER,
+    hostId: DataTypes.INTEGER,
+    bookId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Event',
+    tableName: 'Event',
     underscored: true,
   });
   return Event;
