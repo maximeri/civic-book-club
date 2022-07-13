bd91626c9580f5dbd01d54930106bf0a66734a39
maximeri <t.s.eliot1996@gmail.com>
2022-07-13 15:13:57 +0800

    feat: host static folder public

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
   next()
 })
 app.use('/api/v1', routes)
+app.use(express.static('public'))
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
 
 module.exports = app
